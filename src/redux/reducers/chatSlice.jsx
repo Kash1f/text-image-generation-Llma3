4572c788d10cfee7a9bcf8f5fea13e1d470cb2d5
Kashif <ahmed.kashif1001@gmail.com>
2024-11-14 23:35:51 +0500

    updates

--- src/redux/reducers/chatSlice.jsx
+++ src/redux/reducers/chatSlice.jsx
@@ -6,32 +6,16 @@ const initalState = {
 }
 
 const chatSlice = createSlice({
-  name: 'chat',
-  initialState: initalState,
-  reducers: {
-    setChats: (state, action) => {
-      state.chats = action.payload
-    },
-    setCurrentChatId: (state, action) => {
-      state.currentChatId = action.payload
-    },
-    addChat: (state, action) => {
-      state.chats.push(action.payload)
-    },
-    removeChat: (state, action) => {
-      state.chats = state.chats.filter(chat => chat.id !== action.payload)
-    },
-    updateChat: (state, action) => {
-      const chat = state.chats.find(chat => chat.id === action.payload.id)
-      if (chat) {
-        chat.messages = action.payload.messages
-      }
-    },
-    clearChats: (state) => {
-      state.chats = []
-      state.currentChatId = null
+    name: 'chat',
+    initialState: initalState,
+    reducers: { 
+        addChat: (state, action) => {
+            state.chats.push(action.payload)
+        },
+        setCurrentChatId: (state, action) => {
+            state.currentChatId = action.payload
+        },
     }
-  }
 })
 
-export const { setChats, setCurrentChatId, addChat, removeChat, updateChat, clearChats } = chatSlice.actions
+export const { addChat, setCurrentChatId } = chatSlice.actions      
